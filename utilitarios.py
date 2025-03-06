825147686cf1582678695b97d79cf51ddfbe57ca
niltonmack <nilton.canto@mackenzie.br>
2025-03-05 21:15:15 -0300

    Create utilitarios.py

---
+++ utilitarios.py
@@ -0,0 +1,40 @@
+class Utilitarios:
+    """
+    Classe contendo métodos utilitários implementados por diferentes alunos.
+    """
+
+    def fatorial_brian(self, n):
+        """
+        Calcula o fatorial de um número n (Brian).
+        """
+        pass  # Brian implementará esta função
+
+    def inverte_string_bruno(self, texto):
+        """
+        Inverte uma string (Bruno).
+        """
+        pass  # Bruno implementará esta função
+
+    def fibonacci_samuel(self, n):
+        """
+        Retorna o n-ésimo número da sequência de Fibonacci (Samuel).
+        """
+        pass  # Samuel implementará esta função
+
+    def conta_vogais_lucas(self, texto):
+        """
+        Conta o número de vogais em uma string (Lucas).
+        """
+        pass  # Lucas implementará esta função
+
+    def substring_gabriel(self, texto, inicio, fim):
+        """
+        Retorna uma substring do índice inicio até fim (Gabriel).
+        """
+        pass  # Gabriel implementará esta função
+
+    def palindromo_leonardo(self, texto):
+        """
+        Verifica se um texto é um palíndromo (Leonardo).
+        """
+        pass  # Leonardo implementará esta função
